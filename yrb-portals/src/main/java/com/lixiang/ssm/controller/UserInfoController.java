41fb77b452a36fc70b5c149095166978c0235d5d
tangxiaoting <wifultt_962464zz@163.com>
2018-05-18 16:48:22 +0800

    充值

--- yrb-portals/src/main/java/com/lixiang/ssm/controller/UserInfoController.java
+++ yrb-portals/src/main/java/com/lixiang/ssm/controller/UserInfoController.java
@@ -1,5 +1,6 @@
 package com.lixiang.ssm.controller;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,21 @@ public class UserInfoController {
 	@Autowired
 	private UserInfoService userInfoService;
 	
+	protected Logger log = Logger.getLogger(UserInfoController.class);
+	
+	@RequestMapping("/userInfoToUpdate")
+	public String userInfoToUpdate(UserInfo userInfo,Model model){
+		//1.获取登录者信息，在判断金额，
+		//2.判断银行卡
+		//3.调用service，a. 调用第三方支付接口，如果支付成功，账号里面再加钱
+		userInfoService.updateUserInfos(userInfo);
+		log.error("---------------------"+userInfo);
+		System.out.println("------------------" + userInfo);
+		log.debug("------------------------------------》》》》》");
+		model.addAttribute("userInfo","加钱");
+		return "top-up";
+	}
+	
 	@RequestMapping("/userInfoUpdate")
 	public String userInfoUpdate(UserInfo userInfo,Model model){
 		boolean result=userInfoService.updateUserInfos(userInfo);
@@ -22,5 +38,6 @@ public class UserInfoController {
 		return "redirect:/views/top-up.jsp";
 	}
 	
-
+	
+	
 }
